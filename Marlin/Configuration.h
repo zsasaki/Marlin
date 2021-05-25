76686d45505683a28aab574a8e7de153862a4997
Taisuke SASAKI <zack@ss.iij4u.or.jp>
2021-05-25 21:54:38 +0900

    Enable HS_Mode and change Z axis size.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1058,7 +1058,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -6, -3.20 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -6, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1244,7 +1244,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 15 // Extended max to allow the probe to reach more of the bed.
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 235
+#define Z_MAX_POS 230
 
 /**
  * Software Endstops
